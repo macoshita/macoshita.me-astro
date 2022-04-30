00fc87fa8e4615687fd700c31897b015f6ee95c5
macoshita <macoshita@users.noreply.github.com>
2022-05-01 00:41:44 +0900

    add remark-emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import emoji from "remark-emoji";
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    remarkPlugins: [emoji],
     shikiConfig: {
       // Choose from Shiki's built-in themes
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
